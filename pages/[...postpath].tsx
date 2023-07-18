d32854ff63f9e210ca5df71a8012eedd51bacb9f
RahulShobhnathvishwakarma <127311155+RahulShobhnathvishwakarma@users.noreply.github.com>
2023-07-18 17:41:33 +0530

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://alcashzone.com/` + encodeURI(path as string)
+					`https://www.highwaycpmrevenue.com/pda69u0ui?key=9c92505a5e0799a25fe0df0ff06b810d` 
 				}`,
 			},
 		};
